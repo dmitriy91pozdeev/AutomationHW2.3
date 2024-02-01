ab7484c349429bf8bb1d29cd317dc61ba6e19866
Name Surname <dmitriy91pozdeev@mail.ru>
2024-02-01 11:19:20 +0300

    Test with error

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"));
+        ;
+    }
+}
